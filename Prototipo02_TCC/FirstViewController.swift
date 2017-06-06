9c01831ff32a3cb9ef68250830dafd32c89e8298
matheusmpedrosa <math.pedrosa@hotmail.com>
2017-06-06 12:01:09 -0300

    Initial Commit

---
+++ Prototipo02_TCC/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Prototipo02_TCC
+//
+//  Created by Matheus Pedrosa on 06/06/17.
+//  Copyright © 2017 Matheus Pedrosa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Prototipo02_TCC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Prototipo02_TCC
+//
+//  Created by Matheus Pedrosa on 06/06/17.
+//  Copyright © 2017 Matheus Pedrosa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
